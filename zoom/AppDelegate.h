28f0d7c7f4710dd0baf0f8a972d3e701cd2d5113
Ahmed alfadhli <ahmedalfadhli@Ahmed-alfadhlis-Mac-Pro.local>
2013-02-09 15:19:27 +0300

    Initial Commit

---
+++ zoom/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  zoom
+//
+//  Created by Ahmed alfadhli on 2/9/13.
+//  Copyright (c) 2013 Ahmed alfadhli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ zoom/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  zoom
+//
+//  Created by Ahmed alfadhli on 2/9/13.
+//  Copyright (c) 2013 Ahmed alfadhli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
