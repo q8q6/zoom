416460f64ee67e64c8b90f863bcb6c9ed948c3bf
q8q6 <q8q3q8@gmail.com>
2013-02-26 03:04:56 +0300

    my zoom
    my zooom

--- zoom/ViewController.h
+++ zoom/ViewController.h
@@ -9,5 +9,11 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+ {
+    UIImage *image;
+    UIImageView *img;
+    UIPinchGestureRecognizer *twoFingerPinch;
+}
 
 @end
+ 
--- zoom/ViewController.m
+++ zoom/ViewController.m
@@ -17,9 +17,33 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+ //   zoomInButton=[[UIButton alloc]init];
+ //   zoomOutButton=[[UIButton alloc]init];
+  
+    image = [UIImage imageNamed:@"edit.png"];
+    img = [[UIImageView alloc] initWithImage:image];
+    img.userInteractionEnabled = YES;
+    img.backgroundColor = [UIColor clearColor];
+    
+    img.contentMode =  UIViewContentModeCenter;
+    img.frame = CGRectMake(0, 50, self.view.frame.size.width, self.view.frame.size.height);
+    [self.view addSubview:img];
+    
+    twoFingerPinch = [[UIPinchGestureRecognizer alloc]
+                      initWithTarget:self
+                      action:@selector(twoFingerPinch:)];
+    [img addGestureRecognizer:twoFingerPinch];
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
+- (void)twoFingerPinch:(UIPinchGestureRecognizer *)recognizer
+{
+    //    NSLog(@"Pinch scale: %f", recognizer.scale);
+    if (recognizer.scale >1.0f && recognizer.scale < 2.5f) {
+        CGAffineTransform transform = CGAffineTransformMakeScale(recognizer.scale, recognizer.scale);
+        img.transform = transform;
+    }
+}
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
